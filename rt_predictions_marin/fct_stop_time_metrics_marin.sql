fb14052d1de5bc5d0089c141732e2b6aa7640ff2
tiffanychu90 <tiffany.ku@dot.ca.gov>
2026-04-28 21:51:58 +0000

    add models to run in dbt

---
+++ rt_predictions_marin/fct_stop_time_metrics_marin.sql
@@ -0,0 +1,213 @@
+{{
+    config(
+        materialized='table',
+        cluster_by=['service_date', 'base64_url']
+    )
+}}
+
+WITH int_tu_trip_stop AS (
+    SELECT *
+    FROM `cal-itp-data-infra-staging.tiffany_staging.int_gtfs_rt__trip_updates_trip_stop_day_map_grouping_marin`
+),
+
+trip_stop_with_trip_keys AS (
+    SELECT
+        {{ dbt_utils.generate_surrogate_key([
+            'service_date',
+            'base64_url',
+            'schedule_base64_url',
+            'trip_id',
+            'trip_start_time',
+        ]) }} AS trip_key,
+        key,
+        service_date,
+        base64_url,
+        schedule_base64_url,
+        trip_id,
+        trip_start_time,
+        stop_id,
+        stop_sequence,
+        actual_arrival,
+        actual_departure,
+        actual_arrival_pacific,
+        actual_departure_pacific,
+
+    FROM int_tu_trip_stop
+    GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13
+),
+
+unnested AS (
+    SELECT
+        key,
+        actual_arrival,
+        actual_departure,
+        _extract_ts,
+        arrival_time,
+        departure_time,
+
+        EXTRACT(HOUR FROM _extract_ts) AS extract_hour,
+        EXTRACT(MINUTE FROM _extract_ts) AS extract_minute,
+        DATETIME_DIFF(actual_arrival, _extract_ts, MINUTE) AS minutes_until_arrival,
+        DATETIME_DIFF(actual_arrival, arrival_time, SECOND) AS prediction_seconds_difference_from_arrival,
+        DATETIME_DIFF(actual_departure, arrival_time, SECOND) AS predictions_seconds_difference_from_departure,
+
+    FROM int_tu_trip_stop
+    LEFT JOIN UNNEST(_extract_ts_array) AS _extract_ts WITH OFFSET AS offset0
+    LEFT JOIN UNNEST(arrival_time_array) AS arrival_time WITH OFFSET AS offset1
+    LEFT JOIN UNNEST(departure_time_array) AS departure_time WITH OFFSET AS offset2
+    WHERE offset0 = offset1 AND offset1 = offset2 AND DATETIME_DIFF(actual_arrival, _extract_ts, MINUTE) <= 30 AND  DATETIME_DIFF(actual_arrival, _extract_ts, MINUTE) > 0
+),
+
+prediction_difference AS (
+    SELECT
+        key,
+        extract_hour,
+        extract_minute,
+        minutes_until_arrival,
+
+        arrival_time,
+        actual_arrival,
+        actual_departure,
+
+        -- arrival_time aggregation will be interval type, and we can't take max/min on it, convert to seconds instead
+        (EXTRACT(DAY FROM arrival_time) * 24 * 60 * 60
+         + EXTRACT(HOUR FROM arrival_time) * 60 * 60
+         + EXTRACT(MINUTE FROM arrival_time) * 60
+         + EXTRACT(SECOND FROM arrival_time)) AS arrival_time_seconds,
+
+        -- categorize whether prediction was early/on-time/late and create dummy boolean variables
+        IF(prediction_seconds_difference_from_arrival > 0 AND predictions_seconds_difference_from_departure > 0, TRUE, FALSE) AS is_early,
+        IF(prediction_seconds_difference_from_arrival < 0 AND predictions_seconds_difference_from_departure < 0, TRUE, FALSE) AS is_late,
+        IF(prediction_seconds_difference_from_arrival= 0 OR predictions_seconds_difference_from_departure = 0, TRUE, FALSE) AS is_ontime,
+
+    -- Newmark overwrites some of these as 0 if is_ontime is True and the prediction is compared to actual arrival for early; actual departure for late
+        CASE
+            WHEN (prediction_seconds_difference_from_arrival= 0 OR predictions_seconds_difference_from_departure = 0) THEN 0
+            WHEN (prediction_seconds_difference_from_arrival > 0 AND predictions_seconds_difference_from_departure > 0) THEN prediction_seconds_difference_from_arrival
+            WHEN (prediction_seconds_difference_from_arrival < 0 AND predictions_seconds_difference_from_departure < 0)  THEN predictions_seconds_difference_from_departure
+        END AS prediction_seconds_difference,
+    FROM unnested
+),
+
+minute_bins AS (
+    SELECT
+        key,
+        extract_hour,
+        extract_minute,
+
+        -- wobble metric: https://github.com/cal-itp/data-analyses/blob/main/rt_predictions/03_prediction_inconsistency.ipynb
+        -- Newmark paper does absolute value between observations, but notebook decided to find the max/min per minute and
+        -- set up calculation as max-min to make sure it's always positive
+        -- we want to calculate the spread minute by minute though, so keep max_arrival to compare to lagged prior min_arrival
+        MIN(arrival_time_seconds) AS min_arrival,
+        MAX(arrival_time_seconds) AS max_arrival,
+
+        -- prediction accuracy metric: https://github.com/cal-itp/data-analyses/blob/main/rt_predictions/04_reliable_prediction_accuracy.ipynb
+        AVG(prediction_seconds_difference) AS prediction_error,
+        AVG(minutes_until_arrival) AS minutes_until_arrival,
+
+        COUNTIF(is_early) AS n_predictions_early,
+        COUNTIF(is_ontime) AS n_predictions_ontime,
+        COUNTIF(is_late) AS n_predictions_late,
+
+        -- stop time update completeness metric: https://github.com/cal-itp/data-analyses/blob/main/rt_predictions/01_update_completeness.ipynb
+        COUNT(*) AS n_predictions_minute,
+
+    FROM prediction_difference
+    GROUP BY 1, 2, 3
+),
+
+derive_metrics AS (
+    SELECT
+        key,
+
+        -- 04_reliable_prediction_accuracy.ipynb
+        prediction_error,
+        minutes_until_arrival,
+        IF(prediction_error >= -60 * LN(minutes_until_arrival +1.3)
+                AND prediction_error <= 60* LN(minutes_until_arrival +1.5), 1, 0) AS is_accurate,
+        -- scaled prediction error = prediction_error_sec / seconds_to_arrival
+        ROUND(SAFE_DIVIDE(prediction_error, (minutes_until_arrival * 60)), 3) AS scaled_prediction_error,
+
+        n_predictions_early,
+        n_predictions_ontime,
+        n_predictions_late,
+
+        -- 01_update_completeness.ipynb
+        -- double check this, it's supposed to be fresh update, using header/vehicle_timestamp
+        n_predictions_minute,
+        IF(n_predictions_minute >=2, 1, 0) AS is_complete,
+
+        -- 03_prediction_inconsistency.ipynb.ipynb
+        -- wobble: the max spread is to compare this minute's max arrival with
+        -- prior minute's min arrival
+        -- prior minute: arrival is 9:00 AM, 9:02 AM, 9:01 AM
+        -- current minute: arrival is 9:02 AM, 9:02 AM, 9:01 AM
+        -- the max spread for these 2 minutes for the rider is 2 minutes (9:02 - 9:00)
+        max_arrival - LAG(min_arrival, 1) OVER (
+            PARTITION BY key
+            ORDER BY minutes_until_arrival DESC
+        ) AS prediction_spread_seconds,
+
+    FROM minute_bins
+),
+
+fct_stop_time_metrics AS (
+    SELECT
+        trip_stop_with_trip_keys.trip_key,
+        trip_stop_with_trip_keys.key,
+        trip_stop_with_trip_keys.service_date,
+        trip_stop_with_trip_keys.base64_url,
+        trip_stop_with_trip_keys.schedule_base64_url,
+        trip_stop_with_trip_keys.trip_id,
+        trip_stop_with_trip_keys.trip_start_time,
+
+        trip_stop_with_trip_keys.stop_id,
+        trip_stop_with_trip_keys.stop_sequence,
+        trip_stop_with_trip_keys.actual_arrival,
+        trip_stop_with_trip_keys.actual_departure,
+        trip_stop_with_trip_keys.actual_arrival_pacific,
+        trip_stop_with_trip_keys.actual_departure_pacific,
+
+        -- 04_reliable_prediction_accuracy
+        ROUND(AVG(prediction_error), 2) AS avg_prediction_error_sec,
+        ROUND(AVG(scaled_prediction_error), 3) AS avg_scaled_prediction_error_sec,
+        SUM(is_accurate) AS n_tu_accurate_minutes,
+        SUM(n_predictions_early) AS n_predictions_early,
+        SUM(n_predictions_ontime) AS n_predictions_ontime,
+        SUM(n_predictions_late) AS n_predictions_late,
+
+        -- raw prediction_error per minute is used for percentile plots, bus catch likelihood, and prediction padding metrics
+        ARRAY_AGG(
+            ROUND(prediction_error, 2) IGNORE NULLS -- needed this, but why would this be null?
+            ORDER BY minutes_until_arrival
+        ) AS prediction_error_by_minute_array,
+        -- we need to plot prediction_error against minutes_until_arrival
+        ARRAY_AGG(
+            minutes_until_arrival
+            ORDER BY minutes_until_arrival DESC
+        ) AS minutes_until_arrival_array,
+        ARRAY_AGG(
+            ROUND(scaled_prediction_error, 3) IGNORE NULLS
+            ORDER BY minutes_until_arrival
+        ) AS scaled_prediction_error_by_minute_array,
+
+        -- 01_update_completeness.ipynb
+        SUM(is_complete) AS n_tu_complete_minutes,
+        COUNT(*) AS n_tu_minutes_available,
+
+        -- 03_prediction_inconsistency.ipynb
+        -- convert from prediction_spread_seconds (interval) to minutes
+        COALESCE(SUM(prediction_spread_seconds), 0) AS sum_prediction_spread_seconds, -- wobble
+        MAX(minutes_until_arrival) AS max_minutes_until_arrival, -- this is the denominator
+
+        -- other derived metrics from this prediction window of 30 minutes prior
+        SUM(n_predictions_minute) AS n_predictions,
+
+    FROM derive_metrics
+    INNER JOIN trip_stop_with_trip_keys USING (key)
+    GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13
+)
+
+
+SELECT * FROM fct_stop_time_metrics
---
+++ rt_predictions_marin/fct_trip_updates_trip_metrics_marin.sql
@@ -0,0 +1,58 @@
+{{
+    config(
+        materialized='table',
+        cluster_by=['service_date', 'base64_url']
+    )
+}}
+
+WITH fct_stop_time_metrics AS (
+    SELECT *
+    FROM `cal-itp-data-infra-staging.tiffany_mart_gtfs.fct_stop_time_metrics_marin`
+),
+
+trip_metrics AS (
+    SELECT
+        base64_url,
+        service_date,
+        trip_key, -- use this to join with fct_trip_updates_trip_summaries.key
+
+        AVG(avg_prediction_error_sec) AS avg_prediction_error_sec,
+
+        SUM(n_tu_accurate_minutes) AS n_tu_accurate_minutes,
+        SUM(n_tu_complete_minutes) AS n_tu_complete_minutes,
+
+        SUM(n_tu_minutes_available) AS n_tu_minutes_available,
+        ROUND(
+            SAFE_DIVIDE(
+                SUM(sum_prediction_spread_seconds),
+                SUM(max_minutes_until_arrival)
+        ) / 60, 2) AS avg_prediction_spread_minutes,
+        SUM(n_predictions) AS n_predictions,
+
+        SUM(n_predictions_early) AS n_predictions_early,
+        SUM(n_predictions_ontime) AS n_predictions_ontime,
+        SUM(n_predictions_late) AS n_predictions_late,
+
+        COUNT(DISTINCT key) AS n_tu_stops,
+
+        -- check how combining these arrays (stop_time to stop grain) works for error percentiles
+        -- order by stop_time_key each time so arrays don't lose order when we combine
+        ARRAY_CONCAT_AGG(
+            prediction_error_by_minute_array
+            ORDER BY key
+        ) AS prediction_error_by_minute_array,
+        ARRAY_CONCAT_AGG(
+            scaled_prediction_error_by_minute_array
+            ORDER BY key
+        ) AS scaled_prediction_error_by_minute_array,
+        ARRAY_CONCAT_AGG(
+            minutes_until_arrival_array
+            ORDER BY key
+        ) AS minutes_until_arrival_array,
+
+    FROM fct_stop_time_metrics
+    GROUP BY 1, 2, 3
+    -- aggregate to service_date-trip grain (lose stop_id and stop_sequence)
+)
+
+SELECT * FROM trip_metrics
