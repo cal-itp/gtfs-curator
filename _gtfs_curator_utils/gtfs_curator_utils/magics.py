80b8724ea4ff79b4147b9378271fb49240ada11a
tiffanychu90 <tiffany.ku@dot.ca.gov>
2026-05-06 23:04:03 +0000

    try to bump ipython

--- _gtfs_curator_utils/gtfs_curator_utils/magics.py
+++ _gtfs_curator_utils/gtfs_curator_utils/magics.py
@@ -2,20 +2,10 @@ import json
 
 from IPython import get_ipython
 from IPython.core.magic import register_cell_magic
-from IPython.core.magic_arguments import argument, magic_arguments
 
 
-@magic_arguments()
-@argument(
-    "-m",
-    "--markdown",
-    action="store_true",
-    help="Print the code to markdown, in addition to running",
-)
-@argument("-o", "--output", type=str, help="A variable name to save the result as")
-@argument("-q", "--quiet", action="store_true", help="Whether to hide the result printout")
 @register_cell_magic
-def capture_parameters(line, cell):
+def capture_parameters(cell, magic_name="capture_parameters"):
     shell = get_ipython()
     shell.run_cell(cell, silent=True)
     # We assume the last line is a tuple
