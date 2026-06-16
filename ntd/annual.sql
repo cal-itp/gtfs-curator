aa2ec0d500126b2da97bb654221292c0ef35147c
tiffanychu90 <tiffany.ku@dot.ca.gov>
2026-06-16 16:04:39 +0000

    run in bq with new intermediate agency info

--- ntd/annual.sql
+++ ntd/annual.sql
@@ -1,5 +1,5 @@
 ----------------------------------------------------------
--- (1) tiffany_mart_ntd_explore.fct_service_data_time_series_by_mode
+-- (1) tiffany_mart_ntd_explore.fct_service_data_and_operating_expenses_time_series_by_mode (did not remove keys)
 -- fct_service_data_and_operating_expenses_time_series_by_mode_[one_excel_sheet] feel repetitive
 -- service values: upt, vrh, vrm, voms, pmt, drm (directional_route_miles)
 -- funding values: vo, vm, nvm, ga, total
@@ -73,7 +73,12 @@ int_total AS (
     --{{ ref('int_ntd__service_data_and_operating_expenses_time_series_by_mode_opexp_total') }}
 ),
 
-t1 AS (
+int_agency_information AS (
+    SELECT *
+    FROM `cal-itp-data-infra-staging.tiffany_mart_ntd_explore.int_ntd__service_data_and_operating_expenses_time_series_agency_identifiers`
+),
+
+service_data_and_operating_expenses_time_series_by_mode AS (
     SELECT
         COALESCE(int_upt.key, int_vrh.key, int_vrm.key, int_voms.key, int_pmt.key, int_vo.key, int_vm.key, int_nvm.key, int_ga.key, int_total.key) AS key,
         COALESCE(int_upt.ntd_id, int_vrh.ntd_id, int_vrm.ntd_id, int_voms.ntd_id, int_pmt.ntd_id, int_vo.ntd_id, int_vm.ntd_id, int_nvm.ntd_id, int_ga.ntd_id, int_total.ntd_id) AS ntd_id,
@@ -94,6 +99,20 @@ t1 AS (
         int_ga.opexp_ga AS operating_expenses_general_administration,
         int_total.opexp_total AS operating_expenses_total,
 
+        int_agency_information.agency_status,
+        int_agency_information.census_year,
+        int_agency_information.last_report_year,
+        int_agency_information.mode_status,
+        int_agency_information.reporter_type,
+        int_agency_information.reporting_module,
+        int_agency_information.uace_code,
+        int_agency_information.uza_area_sq_miles,
+        int_agency_information.primary_uza_name,
+        int_agency_information.uza_population,
+        int_agency_information.source_agency,
+        int_agency_information.source_city,
+        int_agency_information.source_state,
+
     FROM int_upt
     LEFT JOIN int_vrh USING (key)
     LEFT JOIN int_vrm USING (key)
@@ -105,31 +124,46 @@ t1 AS (
     LEFT JOIN int_nvm USING (key)
     LEFT JOIN int_ga USING (key)
     LEFT JOIN int_total USING (key)
+    LEFT JOIN int_agency_information USING (key)
 )
 
-SELECT * FROM t1
-
-
-
-
-WHERE key NOT IN ('e41f3812655066d28ec4bbc851545517','f5f160d19e3753e3a99d9ad55b4f2210','7d3e30725b3fa42c6d1722308f9cc855',
+fct_service_data_and_operating_expenses_time_series_by_mode AS (
+    SELECT *
+    FROM service_data_and_operating_expenses_time_series_by_mode
+    WHERE key NOT IN ('e41f3812655066d28ec4bbc851545517','f5f160d19e3753e3a99d9ad55b4f2210','7d3e30725b3fa42c6d1722308f9cc855',
     'da108425cb2696446aa1017bca72340f','a31019318eddb35b747ab79470e10017','98692053a5a16aae8ef8e2579f19b8a3',
     'd6809f84a9d19808f8b1f013fc1cd537','c3ae0b0299c10ffa25e1193404762136','564993fcc3a920cc0800005f3af9fd73',
     '73f01d2aa1c268ec1dafbcf1fdaa84fc','5b13563073a95faa05c9da4f77c0b3a8','0fab2ef186a2a74edc98d16427d4d61a'
+    )
 )
+
+SELECT * FROM fct_service_data_and_operating_expenses_time_series_by_mode
 
 -- step 1 sanity check:
 -- do all the left joins work? do any rows drop? if nothing, can it be inner join? (did both left and inner join, and both got 47_820 rows)
 -- the keys are dbt_utils.generate_surrogate_key(['ntd_id', 'year', 'mode', 'type_of_service']) }}
 
 ----------------------------------------------------------
--- (2) how do agency identifiers differ from dim_agency_information? the intermediatabe table is used for fct
+-- (2) tiffany_mart_ntd_explore.int_ntd__service_data_and_operating_expenses_time_series_agency_identifiers
+-- how do agency identifiers differ from dim_agency_information? the intermediatabe table is used for fct
 -- agency identifiers that show up on each sheet of Excel
 -- dim_agency_information isn't exactly the same, the columns are different
 -- there's dt and execution_ts, which updates every time it hits the view?
 ----------------------------------------------------------
 -- do this here, because int_ tables fix some values, such as NTD_ID
 -- grab upt and opexp_total as representatives
+WITH int_upt AS (
+    SELECT *
+    FROM `cal-itp-data-infra-staging.tiffany_mart_ntd_explore.int_ntd__service_data_and_operating_expenses_time_series_by_mode_upt`
+    --{{ ref('int_ntd__service_data_and_operating_expenses_time_series_by_mode_upt') }}
+),
+
+int_total AS (
+    SELECT *
+    FROM `cal-itp-data-infra-staging.tiffany_mart_ntd_explore.int_ntd__service_data_and_operating_expenses_time_series_by_mode_opexp_total`
+    --{{ ref('int_ntd__service_data_and_operating_expenses_time_series_by_mode_opexp_total') }}
+),
+
 upt_agency_identifiers AS (
     SELECT
         key,
@@ -146,8 +180,7 @@ upt_agency_identifiers AS (
         agency_name AS source_agency,
         city AS source_city,
         state AS source_state,
-        --dt,
-        --execution_ts,
+
     FROM int_upt
     GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14
 ),
@@ -168,29 +201,13 @@ opexp_agency_identifiers AS (
         agency_name AS source_agency,
         city AS source_city,
         state AS source_state,
-        --dt,
-        --execution_ts,
+
     FROM int_total
     GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14
 ),
 
 agency_identifiers AS (
     SELECT * FROM upt_agency_identifiers
-    UNION ALL SELECT * FROM opexp_agency_identifiers
-),
-
-        agency_identifiers.agency_status,
-        agency_identifiers.census_year,
-        agency_identifiers.last_report_year,
-        agency_identifiers.mode_status,
-        agency_identifiers.reporter_type,
-        agency_identifiers.reporting_module,
-        agency_identifiers.uace_code,
-        agency_identifiers.uza_area_sq_miles,
-        agency_identifiers.primary_uza_name,
-        agency_identifiers.uza_population,
-        agency_identifiers.source_agency,
-        agency_identifiers.source_city,
-        agency_identifiers.source_state,
-        --agency_identifiers.dt,
-        --agency_identifiers.execution_ts,
+    UNION DISTINCT SELECT * FROM opexp_agency_identifiers
+)
+SELECT * FROM agency_identifiers
