413d6194306eeaf07fcd4c8282e7b11bd86e7eb4
tiffanychu90 <tiffany.ku@dot.ca.gov>
2026-06-19 18:06:40 +0000

    remove only if ntd_id_2022 is null

--- ntd/annual.sql
+++ ntd/annual.sql
@@ -258,6 +258,74 @@ SELECT * FROM agency_identifiers
 
 ----------------------------------------------------------
 -- (3) crosswalk for ntd_id to bridge's ntd_id_2022 for portfolio deploy
+-- tiffany_mart_ntd_explore.ntd_rtpa_crosswalk2
+----------------------------------------------------------
+-- bridge_gtfs_analysis_name_x_ntd might not be starting point,
+-- the last join takes GTFS operators, then inner joins with org, so if it doesn't have GTFS<
+-- it will never appear
+-- the rest of crosswalk might work, so keep those joins, and see how the bridge table works
+WITH dim_county_geography AS (
+    SELECT DISTINCT
+        key,
+        name,
+        caltrans_district,
+        caltrans_district_name,
+    FROM `cal-itp-data-infra.mart_transit_database.dim_county_geography`
+),
+
+bridge_org_county AS (
+    SELECT
+        organization_key,
+        county_geography_key,
+    FROM `cal-itp-data-infra.mart_transit_database.bridge_organizations_x_headquarters_county_geography`
+),
+
+orgs_with_geog AS (
+    SELECT
+        dim_organizations.source_record_id AS organization_source_record_id,
+        MAX(dim_organizations.name) AS organization_name,
+
+        MAX(dim_county_geography.name) AS county_name,
+        MAX(dim_county_geography.caltrans_district) AS caltrans_district,
+        MAX(dim_county_geography.caltrans_district_name) AS caltrans_district_name,
+
+        MAX(dim_organizations.ntd_id) AS ntd_id,
+        MAX(dim_organizations.ntd_id_2022) AS ntd_id_2022,
+        MAX(dim_organizations.rtpa_name) AS rtpa_name,
+        MAX(dim_organizations.mpo_name) AS mpo_name,
+
+    FROM `cal-itp-data-infra-staging.tiffany_mart_transit_database.dim_organizations_testing` AS dim_organizations
+    INNER JOIN bridge_org_county
+        -- join on organization_key will result in some values with the same
+        -- organization_source_record_id not having ntd_id or rtpa filled in
+        ON dim_organizations.key = bridge_org_county.organization_key
+    INNER JOIN dim_county_geography
+        ON bridge_org_county.county_geography_key = dim_county_geography.key
+    GROUP BY organization_source_record_id
+),
+
+-- annual model will still break LA up into LACMTA and LA County DPW and won't use SCAG
+bridge_split_out_scag AS (
+    SELECT
+        *,
+        CASE
+            WHEN county_name = "Ventura" THEN "Ventura County Transportation Commission"
+            WHEN county_name = "Los Angeles" THEN "Los Angeles County Metropolitan Transportation Authority"
+            WHEN county_name = "San Bernardino" THEN "San Bernardino County Transportation Authority"
+            WHEN county_name = "Riverside" THEN "Riverside County Transportation Commission"
+            WHEN county_name = "Orange" THEN "Orange County Transportation Authority"
+            WHEN county_name = "Imperial" THEN "Imperial County Transportation Commission"
+            ELSE rtpa_name
+        END AS rtpa_name_split
+    FROM orgs_with_geog
+    WHERE ntd_id_2022 IS NOT NULL
+    -- remove orgs that do not have ntd_id populated
+)
+
+SELECT * FROM bridge_split_out_scag
+
+----------------------------------------------------------
+-- (3) crosswalk for ntd_id to bridge's ntd_id_2022 for portfolio deploy
 -- tiffany_mart_ntd_explore.ntd_rtpa_crosswalk
 ----------------------------------------------------------
 WITH bridge AS (
@@ -290,6 +358,7 @@ bridge_split_out_scag AS (
 )
 
 SELECT * FROM bridge_split_out_scag
+
 ----------------------------------------------------------
 -- (4) need macro for mode_full, type_of_service_full
 -- remove seeds: https://github.com/cal-itp/data-infra/blob/main/warehouse/seeds/ntd_modes_to_full_names.csv
