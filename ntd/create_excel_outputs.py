c8a35a0774d4a91d1af5e4173e611279aba3d126
tiffanychu90 <49657200+tiffanychu90@users.noreply.github.com>
2026-06-30 15:42:01 -0700

    try using report aggregation as arg

--- ntd/create_excel_outputs.py
+++ ntd/create_excel_outputs.py
@@ -1,106 +1,90 @@
 """
-Publish NTD monthly ridership by RTPA.
-Publish NTD annual ridership by RTPA (this has multiple files?)
-
-https://github.com/tiffanychu90/curator/blob/test-ntd-data-products/ntd/ntd_utils.py
-save_rtpa_outputs():
-   - handles monthly or annual
-   - both: need RTPA indiv Excel sheets
-   - monthly: 4 sheets: full df, by_agency, by_mode, by_tos (does this upload a year grain? the download step implies it)
-   - annual: 5 sheets: full df, by_agency, by_mode, by_tos, by_reporter_type
-   - both: zip Excel workbook, need public_filename, cover_sheet, upload, remove_local_outputs
+Functions for exporting Excel files
 """
+
+import os
+import shutil
+from pathlib import Path
+
 import gcsfs
 import pandas as pd
+from gtfs_curator_utils import publish_utils
+from update_vars import GCS_FILE_PATH
+
+PUBLIC_GCS = "gs://calitp-publish-data-analysis/"
+fs = gcsfs.GCSFileSystem()
 
 
-def monthly_report_by_rtpa(
-    cover_sheet_path = "cover_sheet_template.xlsx",
-    cover_sheet_index_col = "**NTD Monthly Ridership by RTPA**"
-    indiv_excel_filename = f"{update_vars.YEAR}_{update_vars.MONTH}",
-):
+def readable_rtpa(rtpa_name: str) -> str:
+    return rtpa_name.replace(" ", "_").replace("/", "_").lower()
 
-    excel_output_foldername = f"{indiv_excel_filename}_monthly_report_data"
 
-    monthly_col_dict = {
-        "Uace Cd": "UACE Code",
-        "Dt": "Date",
-        "Tos": "Type of Service",
-        "Legacy Ntd Id": "Legacy NTD ID",
-        "Vrm": "VRM",
-        "Vrh": "VRH",
-        "Voms": "VOMS",
-        "Rtpa": "RTPA",
-        "Pct Change 1Yr": "Percent Change in 1 Year UPT",
-        "Tos Full": "Type of Service Full Name",
-    }
-
-	for one_rtpa in df.rtpa.unique():
-        rtpa_snakecase =
-        
-        with pd.ExcelWriter(f"./{indiv_excel_filename}/{rtpa_snakecase}.xlsx", mode="a") as writer:
-            import_filtered_rtpa_file("monthly_with_crosswalk.parquet", one_rtpa).to_excel(writer, sheet_name="RTPA Ridership", index=False)
-            import_filtered_rtpa_file("monthly/agency.parquet", one_rtpa).to_excel(writer, sheet_name="Aggregated by Agency", index=False)
-            import_filtered_rtpa_file("monthly/mode.parquet", one_rtpa).to_excel(writer, sheet_name="Aggregated by Mode", index=False)
-            import_filtered_rtpa_file("monthly/type_of_service.parquet", one_rtpa).to_excel(writer, sheet_name="Aggregated by TOS", index=False)
-
-    zip_excel(excel_output_foldername)
-    upload_to_gcs()
-    publish_to_public_gcs()
-    remove_local_outputs()
-    return
-
-def import_filtered_rtpa_file(
-    gcs_file_name: str = "",
-    one_rtpa: str
-) -> pd.DataFrame:
+def import_filtered_rtpa_file(gcs_file_name: str = "", one_rtpa: str = "") -> pd.DataFrame:
     df = pd.read_parquet(
-        f"{GCS_FILE_PATH}{gcs_file_name}", 
-        filesystem = gcsfs.GCSFileSystem(),
-        filters = [[("rtpa_name", "==", one_rtpa)]]
+        f"{GCS_FILE_PATH}{gcs_file_name}", filesystem=gcsfs.GCSFileSystem(), filters=[[("rtpa_name", "==", one_rtpa)]]
     ).reset_index(drop=True)
-    
+
     return df
 
-def annual_report_by_rtpa(
-    cover_sheet_path = "annual_cover_sheet_template.xlsx",
-    cover_sheet_index_col = "**NTD Annual Ridership by RTPA**"
-    indiv_excel_filename = f"{update_vars.YEAR}_{update_vars.MONTH}",
-):
-    
-    excel_output_foldername = f"{indiv_excel_filename}_annual_report_data"
-    # TODO: if this is what columns should be, then we should rename in downloaded table, when it's merged with crosswalk 
-    # whatever is downloaded here should be renamed already
-	annual_col_dict = {
-        "source_agency": "agency", 
-        "type_of_service": "tos"
-    }
 
-	for one_rtpa in df.rtpa.unique():
-        rtpa_snakecase =
-        
-        with pd.ExcelWriter(f"./{indiv_excel_filename}/{rtpa_snakecase}.xlsx", mode="a") as writer:
-            import_filtered_rtpa_file("annual_with_crosswalk.parquet", one_rtpa).to_excel(writer, sheet_name="RTPA Ridership", index=False)
-            import_filtered_rtpa_file("annual/agency.parquet", one_rtpa).to_excel(writer, sheet_name="Aggregated by Agency", index=False)
-            import_filtered_rtpa_file("annual/mode.parquet", one_rtpa).to_excel(writer, sheet_name="Aggregated by Mode", index=False)
-            import_filtered_rtpa_file("annual/type_of_service.parquet", one_rtpa).to_excel(writer, sheet_name="Aggregated by TOS", index=False)
-            import_filtered_rtpa_file("annual/reporter_type.parquet", one_rtpa).to_excel(writer, sheet_name="Aggregated by Reporter Type", index=False)
-            
-    zip_excel(excel_output_foldername)
-    upload_to_gcs()
-    publish_to_public_gcs()
-    remove_local_outputs()
+def insert_excel_cover_sheet(report_aggregation: str, excel_output_foldername: str, rtpa_name: str) -> str:
+    """
+    Create Excel workbook for RTPA.
+    Insert cover_sheet.
+    Return the Excel filename of this specific RTPA.
+    """
+    rtpa_snakecase = readable_rtpa(rtpa_name)
+
+    if report_aggregation == "annual":
+        cover_sheet_path = Path("./annual_cover_sheet_template.xlsx")
+        cover_sheet_index_col = "**NTD Annual Ridership by RTPA**"
+
+    elif report_aggregation == "monthly":
+        cover_sheet_path = Path("./monthly_cover_sheet_template.xlsx")
+        cover_sheet_index_col = "**NTD Monthly Ridership by RTPA**"
+
+    # create folder to store each RTPA's Excel
+    Path(excel_output_foldername).mkdir(parents=True, exist_ok=True)
+
+    # define Excel filename for individual RTPA
+    rtpa_excel_filename = Path(f"./{excel_output_foldername}/{rtpa_snakecase}.xlsx")
+
+    # Read in templated cover sheet for RTPA and save out
+    cover_sheet = pd.read_excel(cover_sheet_path, index_col=cover_sheet_index_col, engine="openpyxl")
+    cover_sheet.to_excel(rtpa_excel_filename, sheet_name="README", engine="openpyxl")
+    print(f"created {rtpa_excel_filename}")
+
+    # Need to return the filename created for this RTPA Excel sheet, use in subsequent step
+    return rtpa_excel_filename
+
+
+def export_aggregations_as_excel_sheets(report_aggregation, rtpa_excel_filename, one_rtpa: str):
+    """
+    TODO: if these use report_aggregation, then they have to be programmatically generated in script
+    before this.
+    """
+    with pd.ExcelWriter(rtpa_excel_filename, mode="a") as writer:
+
+        import_filtered_rtpa_file(f"{report_aggregation}_with_crosswalk.parquet", one_rtpa).to_excel(
+            writer, sheet_name="RTPA Ridership", index=False
+        )
+        import_filtered_rtpa_file(f"{report_aggregation}/agency.parquet", one_rtpa).to_excel(
+            writer, sheet_name="Aggregated by Agency", index=False
+        )
+        import_filtered_rtpa_file(f"{report_aggregation}/mode.parquet", one_rtpa).to_excel(
+            writer, sheet_name="Aggregated by Mode", index=False
+        )
+        import_filtered_rtpa_file(f"{report_aggregation}/type_of_service.parquet", one_rtpa).to_excel(
+            writer, sheet_name="Aggregated by TOS", index=False
+        )
+
+        if report_aggregation == "annual":
+            import_filtered_rtpa_file(f"{report_aggregation}/reporter_type.parquet", one_rtpa).to_excel(
+                writer, sheet_name="Aggregated by Reporter Type", index=False
+            )
+
     return
-
 
 def zip_excel(output_file_name):
     shutil.make_archive(f"./{output_file_name}", "zip", output_file_name)
-    return
-
-def upload_to_gcs():
-    fs.upload(f"./{output_file_name}.zip", f"{update_vars.GCS_FILE_PATH}{year}_{month}.zip")
-    return
-
-def publish_to_public_gcs():
-    fs.upload(f"./{output_file_name}.zip", f"{PUBLIC_GCS}ntd_monthly_ridership/{year}_{month}.zip")
     return
