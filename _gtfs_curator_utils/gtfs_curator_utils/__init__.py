f28a4dc4efeae6f1e51f2fe96fa1874c8bd3b09a
tiffanychu90 <tiffany.ku@dot.ca.gov>
2026-07-21 22:58:31 +0000

    remove magics, portfolio_utils, use calitp-portfolio

--- _gtfs_curator_utils/gtfs_curator_utils/__init__.py
+++ _gtfs_curator_utils/gtfs_curator_utils/__init__.py
@@ -3,25 +3,17 @@ from . import (  # magics,
     bq_utils,
     catalog_utils,
     geography_utils,
-    portfolio_utils,
     publish_utils,
     sql,
     utils,
 )
 
 __all__ = [
+    "_color_palette",
     "bq_utils",
     "catalog_utils",
     "geography_utils",
-    "magics",
-    "portfolio_utils",
     "publish_utils",
     "sql",
     "utils",
-    "_color_palette",
 ]
-
-import sys
-
-if "ipykernel" in sys.modules:
-    from . import magics
