ba8882c2e66ab023a13a338ac2591d5bbe691196
tiffanychu90 <tiffany.ku@dot.ca.gov>
2026-08-26 23:10:17 +0000

    fix leading space, rerun scripts, get yamls for all 3

--- gtfs_digest/deploy_district_yaml.py
+++ gtfs_digest/deploy_district_yaml.py
@@ -15,20 +15,19 @@ if __name__ == "__main__":
         site,
         param_key="district",
         values=[
-            "01-Eureka", 
-            "02-Redding",
-            "03-Marysville / Sacramento",
-            "04-Bay Area / Oakland",
-            "05-San Luis Obispo / Santa Barbara",
-            "06-Fresno / Bakersfield",
-            "07-Los Angeles / Ventura",
-            "08-San Bernardino / Riverside", 
-            "09-Bishop", 
-            "10-Stockton", 
-            "11-San Diego",
-            "12-Orange County"
-        ]
+            "01 - Eureka",
+            "02 - Redding",
+            "03 - Marysville / Sacramento",
+            "04 - Bay Area / Oakland",
+            "05 - San Luis Obispo / Santa Barbara",
+            "06 - Fresno / Bakersfield",
+            "07 - Los Angeles / Ventura",
+            "08 - San Bernardino / Riverside",
+            "09 - Bishop",
+            "10 - Stockton",
+            "11 - San Diego",
+            "12 - Orange County",
+        ],
     )
- 
+
     site.write_yaml(SITE_YAML)
-    
--- gtfs_digest/deploy_portfolio_yaml.py
+++ gtfs_digest/deploy_portfolio_yaml.py
@@ -2,56 +2,60 @@
 Create the GTFS Digest yaml that
 sets the parameterization for the analysis site.
 """
+
 from pathlib import Path
 
 import gcsfs
 import pandas as pd
 from calitp_portfolio.models import load_site
 from calitp_portfolio.mutations import generate_parts_grouped
-
 from update_vars import DIGEST_DICT, PROCESSED_GCS, abbrev_month
 
-def generate_operator_report_yaml(
-    site_path: Path,
-    abbrev_month: str
-) -> pd.DataFrame:
+
+def generate_operator_report_yaml(site_path: Path, abbrev_month: str) -> pd.DataFrame:
     """
     Generate the yaml for our Operator grain portfolio.
     """
-    table = DIGEST_DICT.schedule_rt_route_direction
+    table = DIGEST_DICT.operator_summary
     site = load_site(site_path)
 
     # Keep only organizations with RT and schedule OR only schedule.
     df = (
         pd.read_parquet(
-            f"{PROCESSED_GCS}{table}_{abbrev_month}.parquet", 
-            columns=["caltrans_district", "analysis_name"],
-            filesystem = gcsfs.GCSFileSystem()
-        ).drop_duplicates()
+            f"{PROCESSED_GCS}{table}_{abbrev_month}.parquet",
+            columns=["Caltrans District", "Analysis Name"],
+            filesystem=gcsfs.GCSFileSystem(),
+        )
+        .drop_duplicates()
+        .rename(
+            columns={
+                "Caltrans District": "caltrans_district",
+                "Analysis Name": "analysis_name",
+            }
+        )  # rename so it's easier to key into below
         .dropna(subset=["caltrans_district"])
         .reset_index(drop=True)
     )
 
     # To get this to the format groups = {"D1": [1, 2, 3], "D2": [4, 5, 6]}
     operators_grouped_by_district = {
-        one_district: sorted(df[df.caltrans_district==d].analysis_name.unique())
+        f"{one_district.strip()}": sorted(
+            df[df.caltrans_district == one_district].analysis_name.unique()
+        )
         for one_district in sorted(df.caltrans_district.unique())
     }
-    
+
     site = generate_parts_grouped(
-        site,
-        param_key="analysis_name",
-        groups= operators_grouped_by_district
-    })
+        site, param_key="analysis_name", groups=operators_grouped_by_district
+    )
 
     site.write_yaml(site_path)
 
     print(f"yaml generated at {site_path}")
 
-    return 
+    return
+
 
 if __name__ == "__main__":
-
-    SITE_YAML = Path("./gtfs_digest.yml")
-    generate_operator_grain_yaml(SITE_YAML, abbrev_month)
-    
+    SITE_YAML = Path("./operator_digest.yml")
+    generate_operator_report_yaml(SITE_YAML, abbrev_month)
