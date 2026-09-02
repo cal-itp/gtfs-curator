88d59a24a1900d931b4595f3e5a2bfe64c3881f2
tiffanychu90 <tiffany.ku@dot.ca.gov>
2026-09-02 16:54:37 +0000

    try new bridge with dim_annual_agency

--- gtfs_digest/_load_new_ntd.py
+++ gtfs_digest/_load_new_ntd.py
@@ -1,55 +1,84 @@
 import gcsfs
 import google.auth
+import pandas as pd
 from google.cloud import bigquery
 from gtfs_curator_utils import bq_utils
 from update_vars import PROCESSED_GCS
 
-sql_query = """
-    WITH bridge AS (
-        SELECT *
-        FROM `cal-itp-data-infra.mart_transit_database.bridge_gtfs_analysis_name_x_ntd`
-    ),
-
-    dim_ntd AS (
-        SELECT *
-        FROM `cal-itp-data-infra.mart_ntd.dim_annual_agency_information`
-        QUALIFY ROW_NUMBER() OVER (
-        PARTITION BY ntd_id
-        ORDER BY _valid_from DESC
-      ) = 1
-    ),
-
-    bridge_with_ntd AS (
-      SELECT *
-      FROM bridge
-      INNER JOIN dim_ntd
-        ON bridge.ntd_id_2022 = dim_ntd.ntd_id
-    )
-
-    SELECT * FROM bridge_with_ntd
-"""
+credentials, project = google.auth.default()
 
 
 def download_new_ntd_table(
-    project: str,
-    dataset: str,
-    table: str = "test_bridge_with_ntd",
+    min_year: int = 2022,
 ):
-    sql_query = f"""
+    """
+    Set parameter to min_year >= 2022, because we are using ntd_id_2022
+    in crosswalk.
+    """
+    sql_query = """
         SELECT *
-        FROM `{project}.{dataset}.{table}`
+        FROM `cal-itp-data-infra-stag.mart_ntd.dim_annual_agency_information`
+        WHERE year >= @min_year
+        QUALIFY ROW_NUMBER() OVER (
+            PARTITION BY ntd_id
+            ORDER BY _valid_from DESC
+          ) = 1
     """
 
-    df = bq_utils.bq_param_query(sql_query)
-    df.to_parquet(f"{PROCESSED_GCS}{table}.parquet", filesystem=gcsfs.GCSFileSystem())
+    job_config = bigquery.QueryJobConfig(
+        query_parameters=[
+            bigquery.ScalarQueryParameter("min_year", "INT64", min_year),
+        ]
+    )
+    df = bq_utils.bq_param_query(sql_query, job_config=job_config)
+
+    df = prep_ntd(df)
+
+    df.to_parquet(
+        f"{PROCESSED_GCS}dim_annual_agency_information.parquet",
+        filesystem=gcsfs.GCSFileSystem(),
+    )
+
+    print("exported dim_annual_agency_information")
+
     return
 
 
+def prep_ntd(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Set dtypes, rounding.
+    TODO: move this into warehouse, set the dtype there?
+    """
+    integrify_cols = [
+        "year",
+        "zip_code",
+        "zip_code_ext",
+        "service_area_sq_miles",
+        "service_area_pop",
+        "population",
+        "region",
+        "voms_do",
+        "voms_pt",
+        "total_voms",
+        "volunteer_drivers",
+        "personal_vehicles",
+        "number_of_state_counties",
+        "number_of_counties_with_service",
+        "state_admin_funds_expended",
+    ]
+
+    float_cols = ["density", "sq_miles"]
+    df = df.assign(
+        density=df.density.astype(float).round(3),
+    ).astype(
+        {
+            **{c: "Int64" for c in integrify_cols},
+            **{c: "Float64" for c in float_cols},
+        }
+    )
+
+    return df
+
+
 if __name__ == "__main__":
-    credentials, _ = google.auth.default()
-    client = bigquery.Client(
-        project="cal-itp-data-infra-staging", credentials=credentials
-    )
-    download_new_ntd_table(
-        project="cal-itp-data-infra-staging", dataset="tiffany_mart_transit_database"
-    )
+    download_new_ntd_table(min_year=2022)
